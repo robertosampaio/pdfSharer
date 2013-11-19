d322bcbfdba5c55c744fbcc7eaa0100a573accc2
Roberto Sampaio <robertosampaio@amd-novo.(none)>
2013-11-19 15:46:41 -0200

    Projeto criado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PdfSharer::Application.config.secret_key_base = '51296703c2ce8e4ca11b0a9195aa15c04ed089083425560cd57f939a4ddb198fa7b59474a184c40bba0ba7f232a6b3752128704511fb97483147d19088345bbf'
