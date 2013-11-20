6eefa0c35da1e23e0d5bb1b198f57333d0ac26a0
Roberto Sampaio <robertosampaio@amd-novo.(none)>
2013-11-20 10:18:27 -0200

    Gens adicionadas e modelo de user criado

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-PdfSharer::Application.config.secret_key_base = '51296703c2ce8e4ca11b0a9195aa15c04ed089083425560cd57f939a4ddb198fa7b59474a184c40bba0ba7f232a6b3752128704511fb97483147d19088345bbf'
+PdfSharer::Application.config.secret_token = '41b94886fcfa09380f1d17657206f1bd42d408ecbe5506f42a0f7115177616bb25ceaaab9bd2c0617c98069295e187696745633683711ec6369bc53700851b45'
