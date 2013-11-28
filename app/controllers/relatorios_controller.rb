553975c7014e9b4a795ec94bdab7f5c290d9501f
Hugo Tamaki <hugotamaki@p0.(none)>
2013-11-28 13:21:50 -0200

    testando uso do pdfObject

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -1,7 +1,24 @@
+#encoding: utf-8
+
 class RelatoriosController < ApplicationController
   def index
     user = User.find(current_user.id)
     @relatorios = user.relatorios
+    @relatorio = Relatorio.new
+  end
+
+  def create
+    @relatorio = Relatorio.new(params[:relatorio])
+
+    respond_to do |format|
+      if @relatorio.save
+        flash[:notice] = "Arquivo salvo"
+        format.html { redirect_to :back }
+      else
+        flash[:error] = "Arquivo não salvo"
+        format.html { redirect_to :back }
+      end
+    end
   end
 
 end
